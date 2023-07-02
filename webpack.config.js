de73a104ed1a6443f5d382394f96df0d05edf415
Ramon_Mattlov <ramon_mattlov@vulong-development.ru>
2023-07-02 20:04:32 +0300

    Исправлена сборка

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,29 +9,21 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
   },
   devServer: {
     port: 8080,
     historyApiFallback: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
 
   module: {
     rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-typescript'
-              ]
-            }
-          }
-        ]
-      },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
